840d4350c76337f4b06f9959f1fe0606f27d4173
johnsrd7 <johnsrd7@gmail.com>
2017-03-26 18:54:57 -0700

    Update SliceList for Container interface. (#2)

--- lists/slicelist.go
+++ lists/slicelist.go
@@ -1,12 +1,14 @@
-package listcontainer
+package listadts
 
 import (
 	"sync"
+
+	adts "github.com/johnsrd7/go-adts"
 )
 
 // SliceList is a simple type that implements the List interface.
 type SliceList struct {
-	backer       []ListElement
+	backer       []adts.ContainerElement
 	lock         *sync.Mutex
 	threadSafe   bool
 	shrinkFactor float32
@@ -14,12 +16,12 @@ type SliceList struct {
 
 // MakeSliceList creates a new non-threadsafe SliceList.
 func MakeSliceList() *SliceList {
-	return &SliceList{[]ListElement{}, &sync.Mutex{}, false, 0.25}
+	return &SliceList{[]adts.ContainerElement{}, &sync.Mutex{}, false, 0.25}
 }
 
 // MakeSliceListThreadSafe creates a new threadsafe SliceList.
 func MakeSliceListThreadSafe() *SliceList {
-	return &SliceList{[]ListElement{}, &sync.Mutex{}, true, 0.25}
+	return &SliceList{[]adts.ContainerElement{}, &sync.Mutex{}, true, 0.25}
 }
 
 // Cap returns the capacity of the list.
@@ -32,6 +34,10 @@ func (sl *SliceList) Cap() int {
 	return cap(sl.backer)
 }
 
+// -------------------------------------------------------
+// Container Methods
+// -------------------------------------------------------
+
 // Len returns the number of elements in the list.
 func (sl *SliceList) Len() int {
 	if sl.threadSafe {
@@ -42,13 +48,30 @@ func (sl *SliceList) Len() int {
 	return len(sl.backer)
 }
 
-// Contains returns whether the element is in the list.
-func (sl *SliceList) Contains(item ListElement) bool {
-	if sl.Len() > 0 &&
-		sl.Get(0).TypeOf() != item.TypeOf() {
-		return false
+// IsEmpty returns if the list is empty or not.
+func (sl *SliceList) IsEmpty() bool {
+	if sl.threadSafe {
+		sl.lock.Lock()
+		defer sl.lock.Unlock()
+		return sl.Len() == 0
 	}
 
+	return sl.Len() == 0
+}
+
+// Clear clears out the list.
+func (sl *SliceList) Clear() {
+	if sl.threadSafe {
+		sl.lock.Lock()
+		defer sl.lock.Unlock()
+		sl.backer = []adts.ContainerElement{}
+	}
+
+	sl.backer = []adts.ContainerElement{}
+}
+
+// Contains returns whether the element is in the list.
+func (sl *SliceList) Contains(item adts.ContainerElement) bool {
 	if sl.threadSafe {
 		sl.lock.Lock()
 		defer sl.lock.Unlock()
@@ -61,7 +84,7 @@ func (sl *SliceList) Contains(item ListElement) bool {
 // containsHelper returns whether the given element is in the list. Doesn't
 // check to see if the element is of the proper type. It is assumed that
 // the type check is done before the call to this method.
-func (sl *SliceList) containsHelper(item ListElement) bool {
+func (sl *SliceList) containsHelper(item adts.ContainerElement) bool {
 	for _, i := range sl.backer {
 		if i.Equals(item) {
 			return true
@@ -72,11 +95,7 @@ func (sl *SliceList) containsHelper(item ListElement) bool {
 }
 
 // Add returns true if the given element was appended to the end of the list.
-func (sl *SliceList) Add(item ListElement) bool {
-	if sl.Len() > 0 && sl.Get(0).TypeOf() != item.TypeOf() {
-		return false
-	}
-
+func (sl *SliceList) Add(item adts.ContainerElement) bool {
 	if sl.threadSafe {
 		sl.lock.Lock()
 		defer sl.lock.Unlock()
@@ -90,20 +109,37 @@ func (sl *SliceList) Add(item ListElement) bool {
 
 // Remove removes the element at the given index and
 // returns whether the removal was successful.
-func (sl *SliceList) Remove(idx int) bool {
-	if idx >= sl.Len() {
-		return false
-	}
-
+func (sl *SliceList) Remove(item adts.ContainerElement) bool {
 	if sl.threadSafe {
 		sl.lock.Lock()
 		defer sl.lock.Unlock()
+		idx := sl.findHelper(item)
+		if idx < 0 {
+			return false
+		}
 		sl.removeHelper(idx)
 	} else {
+		idx := sl.findHelper(item)
+		if idx < 0 {
+			return false
+		}
 		sl.removeHelper(idx)
 	}
 
 	return true
+}
+
+// findHelper searches the list for the given element and returns the index
+// of the item in the list. If the item doesn't exist in the list, then -1
+// is returned.
+func (sl *SliceList) findHelper(item adts.ContainerElement) int {
+	for idx, val := range sl.backer {
+		if item.Equals(val) {
+			return idx
+		}
+	}
+
+	return -1
 }
 
 // removeHelper removes the element at the given idx.
@@ -118,14 +154,18 @@ func (sl *SliceList) removeHelper(idx int) {
 	if emptyFactor <= sl.shrinkFactor {
 		// Shrink the slice's capacity by 1/2
 		newCap := cap(sl.backer) / 2
-		newBacker := make([]ListElement, len(sl.backer), newCap)
+		newBacker := make([]adts.ContainerElement, len(sl.backer), newCap)
 		copy(newBacker, sl.backer)
 		sl.backer = newBacker
 	}
 }
 
+// -------------------------------------------------------
+// List Methods
+// -------------------------------------------------------
+
 // Get returns the element at the given index.
-func (sl *SliceList) Get(idx int) ListElement {
+func (sl *SliceList) Get(idx int) adts.ContainerElement {
 	if sl.threadSafe {
 		sl.lock.Lock()
 		defer sl.lock.Unlock()
@@ -133,4 +173,20 @@ func (sl *SliceList) Get(idx int) ListElement {
 	}
 
 	return sl.backer[idx]
+}
+
+// Set changes the value at the given index to the given new value
+// and returns the old value that was at the given index.
+func (sl *SliceList) Set(idx int, newVal adts.ContainerElement) adts.ContainerElement {
+	if sl.threadSafe {
+		sl.lock.Lock()
+		defer sl.lock.Unlock()
+		oldVal := sl.backer[idx]
+		sl.backer[idx] = newVal
+		return oldVal
+	}
+
+	oldVal := sl.backer[idx]
+	sl.backer[idx] = newVal
+	return oldVal
 }
